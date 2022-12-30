b9b00d0a007f222d368dccee8d8de55e2b15efef
Rafael Jesus <rafaelljesus86@gmail.com>
2022-12-31 00:14:40 +0100

    implement lifo

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
-pub mod fifo;
+pub mod queue;
 pub mod rr;
 
 #[cfg(test)]
 mod tests {
-    use crate::fifo::Fifo;
+    use crate::queue::{Kind, Queue};
     use crate::rr::RR;
 
     #[test]
-    fn rr() {
+    fn random_replacement() {
         let mut rr_cache = RR::new(3);
         assert_eq!(rr_cache.get(1), None);
         assert_eq!(rr_cache.set(1, "one"), true);
@@ -20,8 +20,8 @@ mod tests {
     }
 
     #[test]
-    fn fifo() {
-        let mut fifo_cache: Fifo<i32, &str> = Fifo::new(3);
+    fn queue_fifo() {
+        let mut fifo_cache: Queue<i32, &str> = Queue::new(3, Kind::FIFO);
         assert_eq!(fifo_cache.get(1), None);
         assert_eq!(fifo_cache.set(1, "one"), true);
         assert_eq!(fifo_cache.get(1), Some(&"one"));
@@ -30,5 +30,18 @@ mod tests {
         assert_eq!(fifo_cache.set(3, "three"), true);
         assert_eq!(fifo_cache.set(4, "four"), true);
         assert!(fifo_cache.get(2).is_some());
+    }
+
+    #[test]
+    fn queue_lifo() {
+        let mut lifo_cache: Queue<i32, &str> = Queue::new(3, Kind::LIFO);
+        assert_eq!(lifo_cache.get(1), None);
+        assert_eq!(lifo_cache.set(1, "one"), true);
+        assert_eq!(lifo_cache.get(1), Some(&"one"));
+
+        assert_eq!(lifo_cache.set(2, "two"), true);
+        assert_eq!(lifo_cache.set(3, "three"), true);
+        assert_eq!(lifo_cache.set(4, "four"), true);
+        assert!(lifo_cache.get(2).is_some());
     }
 }
--- src/queue.rs
+++ src/queue.rs
@@ -4,8 +4,10 @@ use std::{
     hash::Hash,
 };
 
-/// The FIFO cache evicts the items in the order they were added
+/// The queue-based cache implements the FIFO and LIFO queue algorithms
+/// FIFO cache evicts the items in the order they were added
 /// without any regard to how often or how many times they were accessed before
+/// LIFO cache evicts items added most recently first without any regard to how often or how many times it was accessed before.
 pub struct Queue<K, V> {
     entry_map: HashMap<K, V>,
     keys: VecDeque<K>,
@@ -25,18 +27,29 @@ where
         Self {
             entry_map: HashMap::with_capacity(capacity),
             keys: VecDeque::with_capacity(capacity),
-            kind: Kind,
+            kind: kind,
         }
     }
 
     // Time: O(1) | Space: O(n)
     pub fn set(&mut self, key: K, value: V) -> bool {
         if self.entry_map.capacity() == self.entry_map.len() {
-            let front_key = match self.keys.pop_front() {
-                Some(front_key) => front_key,
-                None => return false,
-            };
-            match self.entry_map.remove_entry(&front_key) {
+            let key_to_evict;
+            match self.kind {
+                Kind::FIFO => {
+                    key_to_evict = match self.keys.pop_front() {
+                        Some(front_key) => front_key,
+                        None => return false,
+                    };
+                }
+                Kind::LIFO => {
+                    key_to_evict = match self.keys.pop_back() {
+                        Some(back_key) => back_key,
+                        None => return false,
+                    };
+                }
+            }
+            match self.entry_map.remove_entry(&key_to_evict) {
                 Some((_, _)) => (),
                 // maybe it should panic if entry not present?
                 None => return false,
